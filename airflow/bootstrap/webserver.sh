624506143bc733d3b00bac5a8e15ffc98493ed2b
David Gold <david.gold.a@gmail.com>
2022-01-07 19:48:31 -0800

    Don't webserver

--- airflow/bootstrap/webserver.sh
+++ airflow/bootstrap/webserver.sh
@@ -8,14 +8,4 @@ airflow db init
 airflow users create -r Admin -u admin -f FirstName -l LastName -p ${ADMIN_PASS} -e admin@test.com
 # sleep 5
 
-# sudo mount \
-#     -t nfs4 \
-#     -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport \
-#     ${EFS_FILE_SYSTEM_ID}.efs.${AWS_REGION}.amazonaws.com:/ \
-#     ${MOUNT_POINT}
-
-# sudo mount -t efs ${EFS_FILE_SYSTEM_ID}:/ /mount/efs
-
-df -T
-
-airflow webserver
+# airflow webserver
