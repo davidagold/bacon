2959058e9d20f3491d0204b1c4fa418334919e20
David Gold <david.gold.a@gmail.com>
2022-01-13 17:15:28 -0800

    Just wandb

--- exp/sweep/init.sh
+++ exp/sweep/init.sh
@@ -2,14 +2,5 @@
 
 set -Eeuxo pipefail
 
-echo "
-[mount]
-region=${AWS_REGION}" | crudini --merge /etc/amazon/efs/efs-utils.conf
-
-cat /etc/exports 
-
-mount -t efs ${EFS_FILE_SYSTEM_ID}:/ ${MOUNT_POINT}
-
-ls ${SWEEP_DIR}
 
 wandb login
